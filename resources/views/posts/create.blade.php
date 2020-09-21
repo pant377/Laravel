c15c45ff9b36270e1d95e7b26b22ff0dcfc6eda9
pant377 <pantpap15@gmail.com>
2020-09-21 19:22:25 +0300

    this is the thing

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,5 +2,15 @@
 
 @section('content')
     <h1>Create Some Posts</h1>
-         
+    {!! Form::open(['action' => 'App\Http\Controllers\PostController@store', 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title','Title')}}
+            {{Form::text('title', '',['class' => 'form-control','placeholder' => 'Title'])}}
+        </div>   
+        <div class="form-group">
+            {{Form::label('body','body')}}
+            {{Form::text('body', '',['class' => 'form-control','placeholder' => 'bodytext'])}}
+        </div>
+        {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+    {!! Form::close() !!}
 @endsection
